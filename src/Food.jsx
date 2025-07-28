c09065d9b12220daa9ad51fb0f5c7f6d166e2650
Antonio Poloni <antoniopolonijr@gmail.com>
2025-07-28 14:58:23 -0300

    feat: create Food component. Closes #1

---
+++ src/Food.jsx
@@ -0,0 +1,11 @@
+const Food = (props) => {
+  return (
+    <div className="food">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
+};
+
+export default Food;
